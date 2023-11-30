2c3f2c51f48000aff8160007fc05bf9b1e82dff1
Dierta Pasific <diertapasific@gmail.com>
2023-11-30 17:57:20 +0700

    Initial Commit

---
+++ VeganFuel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeganFuel
+//
+//  Created by Dierta Pasific on 30/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
